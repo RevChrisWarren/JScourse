ebbd054d7f04d5ef9515c79df030c2bbd3254484
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-10-11 18:52:01 -0500

    dom manipulation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -31,3 +31,17 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+const allSection = document.querySelectorAll('.section')
+console.log(allSection);
+
+document.getElementById('section-1')
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
