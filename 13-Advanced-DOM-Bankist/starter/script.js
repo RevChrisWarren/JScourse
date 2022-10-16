add9853c3e4ef0a4d4f09ccf4646b0da4235c46b
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-10-15 21:05:32 -0500

    new way to do sticky nav bar

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -127,14 +127,16 @@ nav.addEventListener('mouseover', handleHover.bind(0.5))
 nav.addEventListener('mouseout', handleHover.bind(1))
 
 //Sticky Navigation
-const initialCoords = section1.getBoundingClientRect()
+// const initialCoords = section1.getBoundingClientRect()
 
-console.log(initialCoords);
-window.addEventListener('scroll', function (e) {
-  console.log(window.scrollY);
-  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
-  else nav.classList.remove('sticky')
-})
+// console.log(initialCoords);
+// window.addEventListener('scroll', function (e) {
+//   console.log(window.scrollY);
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky')
+// })
+
+//STICKY NAV WITH INTERSECTION OBSERVER API
 
 ///////////////////////////////////////////
 ///////////////////////////////////////////
