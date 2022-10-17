0200abccad4d0c6b5d185c868c4361700a3545b4
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-10-16 21:21:47 -0500

    lazy oading of images implemented

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -178,7 +178,7 @@ const allSections = document.querySelectorAll('.section')
 
 const sectionReveal = function (entries, observer) {
   const [entry] = entries
-  console.log(entry);
+  // console.log(entry);
   if (!entry.isIntersecting) return
 
   entry.target.classList.remove('section--hidden');
@@ -189,6 +189,7 @@ const sectionReveal = function (entries, observer) {
 const sectionRevealOptions = {
   root: null,
   threshold: 0.15,
+
 }
 
 const sectionObserver = new IntersectionObserver(sectionReveal, sectionRevealOptions);
@@ -196,6 +197,34 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section)
   section.classList.add('section--hidden')
 })
+
+//lazy loading images
+
+const imgTargets = document.querySelectorAll('img[data-src]')
+console.log(imgTargets);
+
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  //replace with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  })
+  observer.unobserve(entry.target);
+}
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+})
+
+imgTargets.forEach(img => imgObserver.observe(img))
+
 ///////////////////////////////////////////
 ///////////////////////////////////////////
 /*
