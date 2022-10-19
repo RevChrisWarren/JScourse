32d71dee900710d9bf63ea0699e6b27b8da020f6
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-10-18 21:19:15 -0500

    beginning with OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // //NEVER DO THIS!!!
+    // this.calcAge = function () {
+    //     console.log(2022 - this.birthYear)
+    // }
+
+}
+
+const chris = new Person('Chris', 1974);
+console.log(chris);
+
+//because called with 'new'
+// 1. New empty object {} was created
+// 2. Function wacs called, and this = new empty object {}
+// 3. new object {} linked to a prototype
+// 4. object automatically returned from the constructor function
+
+const joy = new Person('Joy', 1974)
+console.log(joy);
+
+console.log(chris instanceof Person);
