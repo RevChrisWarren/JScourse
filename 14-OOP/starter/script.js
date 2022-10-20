c3173ae1e0acb7c593b82cdb759b3aa629e55c6d
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-10-19 20:44:28 -0500

    prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -13,7 +13,7 @@ const Person = function (firstName, birthYear) {
 }
 
 const chris = new Person('Chris', 1974);
-console.log(chris);
+// console.log(chris);
 
 //because called with 'new'
 // 1. New empty object {} was created
@@ -21,7 +21,27 @@ console.log(chris);
 // 3. new object {} linked to a prototype
 // 4. object automatically returned from the constructor function
 
-const joy = new Person('Joy', 1974)
-console.log(joy);
+const joy = new Person('Joy', 1984)
+// console.log(joy);
 
-console.log(chris instanceof Person);
+
+// console.log(chris instanceof Person);
+
+//PROTOTYPES
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+    console.log(2022 - this.birthYear)
+}
+
+joy.calcAge();//Access to this because of prototypal inheritance
+chris.calcAge();
+
+console.log(chris.__proto__); //shows the prototype properties available
+console.log(chris.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(joy));
+console.log(Person.prototype.isPrototypeOf(chris));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+//May be better to call this prototype of linked object
