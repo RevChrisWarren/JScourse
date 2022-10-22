61a9b65dd73e619baba79f0be0032ca27a29cb7a
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-10-21 20:48:27 -0500

    getters and setters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -114,17 +114,32 @@ mercedes.brake()
 
 //class declaration
 class PersonCl {
-    constructor(firstName, birthYear) {
-        this.firstName = firstName;
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
         this.birthYear = birthYear;
     }
     //This will be on the prototype, not on the object itself
     calcAge() {
         console.log(2022 - this.birthYear);
     }
+    get age() {
+        return 2022 - this.birthYear
+    }
+
+    //Common pattern for settign property that already exists
+    set fullName(name) {
+        console.log(name);
+        if (name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name.`)
+    }
+
+    get fullName() {
+        return this._fullName
+    }
 }
 
-const jessica = new PersonCl('Jessica', 1989);
+
+const jessica = new PersonCl('Jessica Davis', 1989);
 
 console.log(jessica);
 jessica.calcAge();
@@ -133,10 +148,36 @@ console.log(jessica.__proto__ === PersonCl.prototype);
 PersonCl.prototype.greet = function () {
     console.log(`Hey, ${this.firstName}`);
 }
-
+console.log(jessica.age)
 jessica.greet()
 
+
+const walter = new PersonCl('Walter Jones', 1958)
+
+console.log(walter.age)
 //NOTES:
 // 1. Classes are not hoisted
 // 2. Classes are first class citizens like functions
 // 3. The body of the class is always executed in strict mode
+
+//SETTERS AND GETTERS -functions that get and set a value
+
+const account = {
+    owner: 'jonas',
+    movements: [200, 350, 600, -400],
+
+    get latest() {
+        return this.movements.slice(-1).pop()
+    },
+    set latest(mov) {
+        this.movements.push(mov);
+    }
+
+};
+//don't call method, just use as if a property
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
+
+
