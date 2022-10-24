5d1a003d2fc30ec6351635c6c8c25d7e1d7461e2
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-10-24 18:57:28 -0500

    lat, long

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -13,7 +13,11 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(function (position) {
-        console.log(position);
+        const { latitude } = position.coords
+        const { longitude } = position.coords
+        console.log(latitude, longitude);
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
     }, function () {
         alert('Could not get your position.')
-    })
+    });
+
