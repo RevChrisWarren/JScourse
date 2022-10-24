6098cdbf4b948989609090e0c0272324d6173e43
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-10-23 20:53:24 -0500

    encapsulation conventions

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -437,25 +437,31 @@ class Account {
     constructor(owner, currency, pin) {
         this.owner = owner;
         this.currency = currency;
-        this.pin = pin;
-        this.movements = [];
+
+        this._pin = pin;
+        //Protected property
+        this._movements = [];
         this.locale = navigator.language
         console.log(`Thanks for opening an account ${owner}!`);
     }
     //Public Interface of objects
+    getMovements() {
+        return this._movements;
+    }
     deposit(val) {
-        this.movements.push(val)
+        this._movements.push(val)
     }
     withdrawal(val) {
         this.deposit(-val)
     }
-    approveLoan(val) {
+    _approveLoan(val) {
         return true;
     }
     requestLoan(val) {
-        if (this.approveLoan(val))
+        if (this._approveLoan(val)) {
             this.deposit(val)
-        console.log('Loan Approved');
+            console.log('Loan Approved');
+        }
     }
 }
 
@@ -471,3 +477,9 @@ acct1.withdrawal(150)
 acct1.requestLoan(1000)
 
 console.log(acct1);
+
+//Encapsulation convention
+
+console.log(acct1.getMovements())
+console.log(acct1.pin);
+
