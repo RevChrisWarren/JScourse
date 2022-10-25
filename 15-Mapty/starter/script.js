79b93610f62dbdeac9cfe94fab33f979128091b1
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-10-24 19:43:47 -0500

    setting up map popups

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -28,12 +28,22 @@ if (navigator.geolocation)
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>contributors'
         }).addTo(map);
 
-        L.marker(coords)
-            .addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
+        // L.marker(coords)
+        //     .addTo(map)
+        //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        //     .openPopup();
 
-        // map.on()
+        map.on('click', function (mapEvent) {
+            console.log(mapEvent);
+
+            const { lat, lng } = mapEvent.latlng
+            L.marker([lat, lng])
+                .addTo(map)
+                .bindPopup(L.popup({
+
+                }))
+                .openPopup();
+        })
 
     }, function () {
         alert('Could not get your position.')
