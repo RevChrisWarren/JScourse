c34db7cd107e34fa56e75c0021cab1a949cd6082
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-10-26 21:56:34 -0500

    added reset method

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -304,6 +304,10 @@ class App {
             this._renderWorkout(work);
         })
     }
+    reset() {
+        localStorage.removeItem('workouts');
+        location.reload();
+    }
 }
 
 const app = new App();
