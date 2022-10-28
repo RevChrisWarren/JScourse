e10713bdae77a04441502568076e1ccc1f21bb64
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-10-28 15:17:26 -0500

    chaining promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -101,6 +101,36 @@ setTimeout(() => {
 // request.open('GET', `https://restcountries.com/v2/name/${country}`);
 // request.send();
 
+const renderCountry = function (data, className = '') {
+    const html = ` <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+}
 const request = fetch(`https://restcountries.com/v2/name/portugal`)
 
-console.log(request);
+// console.log(request);
+
+const getCountryData = function (country) {
+    //Country 1
+    fetch(`https://restcountries.com/v2/name/${country}`)
+        .then(response => response.json())
+        .then(data => {
+            renderCountry(data[0]);
+            const neighbor = data[0].borders[0];
+            if (!neighbor) return;
+            //Country 2 RETURN SO YOU CAN HANDLE OUTSIDE THE PREVIOUS .THEN FUNCTION
+            return fetch(`https://restcountries.com/v2/alpha/${neighbor}`)
+        })
+        .then(response => response.json())
+        .then(data => renderCountry(data, 'neighbour'))
+};
+getCountryData('usa')
