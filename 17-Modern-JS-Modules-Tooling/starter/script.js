9f8812b0f3a70a19f86e004d6d8a9858d92c0a98
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-11-02 21:24:30 -0500

    older module pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,6 +1,7 @@
 //Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js'
 // addToCart('bread', 5)
+/*
 console.log('Importing module');
 // console.log(price, tq);
 
@@ -46,3 +47,31 @@ lastPost.then(res => console.log(res))
 const lastPost2 = await getLastPost()
 console.log(lastPost2);
 
+
+
+const ShoppingCart2 = (function () {
+	const cart = [];
+	const shippingCost = 10;
+	const totalPrice = 237;
+	const totalQuantity = 24;
+	const addToCart = function (product, quantity) {
+		cart.push({ product, quantity });
+		console.log(`${quantity} ${product} added to cart(shipping cost is ${shippingCost})`);
+	}
+	const orderStock = function (product, quantity) {
+		console.log(`${quantity} ${product} ordered from supplier`);
+	};
+	return {
+		addToCart,
+		cart,
+		totalPrice,
+		totalQuantity
+	}
+})();
+
+ShoppingCart2.addToCart('apple', 4)
+ShoppingCart2.addToCart('pizza', 2)
+
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
+*/
